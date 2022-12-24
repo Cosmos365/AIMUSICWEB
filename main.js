5142c074bf0899648180d6ba6d4ee8b78f59a116
Avi <81462673+Cosmos365@users.noreply.github.com>
2022-12-24 18:14:49 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(515, 150);
+}
+ 
+
+function draw() {
+    background('#969A97');
+}
